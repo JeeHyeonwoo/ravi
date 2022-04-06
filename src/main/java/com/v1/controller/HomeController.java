54d93cfbfc11314082294105cc44dc5c1637c3b7
jeehyunu <cosmo242@nate.com>
2022-04-06 18:03:08 +0900

    Calendar

--- src/main/java/com/v1/controller/HomeController.java
+++ src/main/java/com/v1/controller/HomeController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/test")
+    @GetMapping("/index")
     public String test() {
-        return "/test";
+        return "index";
     }
 }
